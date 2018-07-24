d880e02e7b35b67ed1b60145951d43ae26ed06d5
Jwarburton1987 <jamesforrealestate@gmail.com>
2018-07-23 17:31:22 -0700

    furst session

---
+++ friendFinder/app/data/friends.js
@@ -0,0 +1,28 @@
+// var dummyData = {
+//     "character":"Jesus",
+//     "image":"https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi3ybPXhbbcAhUpjlQKHXzgA88QjRx6BAgBEAU&url=https%3A%2F%2Fcommons.wikimedia.org%2Fwiki%2FFile%3AJesus_of_Macedonia_(1262).jpeg&psig=AOvVaw2OPixx37GwPVvTMEBMalIU&ust=1532463536261043",
+//     "truths": {
+//         "strength" : 100,
+//         "intelligenece" : 100,
+//         "age_of_death" : "unknown"
+//     }
+    
+  
+// }
+var dummyData = {
+    "name":"Ahmed",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  }
+  
